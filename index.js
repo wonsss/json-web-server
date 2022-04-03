7caafdd820a36c147c5d0b52254085b5478e4a7d
Marco <bigdatajang@gmail.com>
2022-04-03 15:34:37 +0900

    cors 추가

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const jsonServer = require("json-server");
 const auth = require("json-server-auth");
+const cors = require("cors");
 
 const app = jsonServer.create();
 const router = jsonServer.router("db.json");
